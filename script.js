db47cf7bc9f0e39cb7413272c5b97eae3cb22145
James Boase <jamesboase123@gmail.com>
2023-07-03 12:10:56 -0400

    create each file and add math functions to script

---
+++ script.js
@@ -0,0 +1,19 @@
+let a = 0;
+let b = 0;
+let operation = "";
+
+function add(a, b){
+    return a + b;
+}
+
+function subtract(a, b){
+    return a - b;
+}
+
+function multiply(a, b){
+    return a * b;
+}
+
+function divide(a, b){
+    return a / b;
+}
