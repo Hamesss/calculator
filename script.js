c1647434116295f8d6931f9ab31667bb6164bd0e
James Boase <jamesboase123@gmail.com>
2023-07-03 12:45:39 -0400

    display selected button on display

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 let a = 0;
 let b = 0;
 let operation = "";
+const btns = document.querySelectorAll("button");
+const displayText = document.querySelector(".display-text")
+btns.forEach(button => button.addEventListener("click", updateDisplay));
+function updateDisplay(e){
+    if (this.innerHTML == "Clear"){
+        displayText.innerHTML = "";
+    }
+    else{
+    displayText.innerHTML = this.innerHTML;
+    }
+}
+
+
+
 
 function operate(operation, a, b){
     let result = 0;
