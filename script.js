eca5c5f98072a02b3bdcd4d216ad5ab7034cb848
James Boase <jamesboase123@gmail.com>
2023-07-03 12:22:04 -0400

    add function to choose operation and add calculator buttons

--- script.js
+++ script.js
@@ -2,6 +2,23 @@ let a = 0;
 let b = 0;
 let operation = "";
 
+function operate(operation, a, b){
+    let result = 0;
+    if (operation == "+"){
+        result = add(a, b);
+    }
+    else if (operation == "-"){
+        result = subtract(a, b);
+    }
+    else if (operation == "*"){
+        result = multiply(a, b);
+    }
+    else if (operation == "/"){
+        result = divide(a, b);
+    }
+    return result;
+}
+
 function add(a, b){
     return a + b;
 }
